aa9f3fea849b00ba8b42650b88dfe6aa51a9312e
SebastianFg <fernandesdeguimaraes@gmail.com>
2019-01-02 21:27:19 -0300

    Prmer Comit

---
+++ src/app/shared/nopagesfund/nopagesfund.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagesfund',
+  templateUrl: './nopagesfund.component.html',
+  styles: []
+})
+export class NopagesfundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
